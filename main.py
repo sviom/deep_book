855ec571df7e28836f8e9475a806d29ab63a1bb0
kanghanstar <kanghanstar@gmail.com>
2024-03-14 23:06:00 +0900

    NAND, OR, AND 게이트 추가

--- main.py
+++ main.py
@@ -1,14 +1,68 @@
 import numpy as np
-from matplotlib import pyplot as plt, image
 
 
-# x = np.arange(0,6, 0.1)
-# y = np.sin(x)
+def ANDGate(x1, x2):
+    zz = np.array([x1, x2])
+    kk = np.array([0.5, 0.5])
+    gg = np.sum(zz * kk)
+    b = -0.7
+    tmp = gg + b
+    if tmp <= 0:
+        print(str(0))
+        return 0
+    elif tmp > 0:
+        print(str(1))
+        return 1
 
-# plt.plot(x, y)
-# plt.show()
 
-img = image.imread('./images/pepetest.jpg')
+def NANDGate(x1, x2):
+    zz = np.array([x1, x2])
+    kk = np.array([-0.5, -0.5])
+    gg = np.sum(zz * kk)
+    b = 0.7
+    tmp = gg + b
+    if tmp <= 0:
+        print(str(0))
+        return 0
+    elif tmp > 0:
+        print(str(1))
+        return 1
 
-plt.imshow(img)
-plt.show()
+
+def ORGate(x1, x2):
+    zz = np.array([x1, x2])
+    kk = np.array([0.5, 0.5])
+    gg = np.sum(zz * kk)
+    b = -0.2
+    tmp = gg + b
+    if tmp <= 0:
+        print(str(0))
+        return 0
+    elif tmp > 0:
+        print(str(1))
+        return 1
+
+
+ANDGate(0, 0)
+ANDGate(0, 1)
+ANDGate(1, 0)
+ANDGate(1, 1)
+
+NANDGate(0, 0)
+NANDGate(0, 1)
+NANDGate(1, 0)
+NANDGate(1, 1)
+
+ORGate(0, 0)
+ORGate(0, 1)
+ORGate(1, 0)
+ORGate(1, 1)
+
+# x = np.array([0, 1])
+# w = np.array([0.5, 0.5])
+# b = -0.7
+
+# z = x * w
+# y = np.sum(z)
+# k = y + b
+# print(k)
